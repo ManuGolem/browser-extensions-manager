675129d57acc175c7922213b4554985017328a84
ManuGolem <manuelcarreras2013@gmail.com>
2025-04-15 22:33:45 -0300

    add change event to input to deteck when active the extension

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 const botonesFiltros = document.querySelectorAll("li");
 const container = document.querySelector(".container");
 document.addEventListener("DOMContentLoaded", () => {
+    //Traer los datos del .json
+    fetch("../data.json")
+        .then((response) => response.json())
+        .then((data) => init(data));
+});
+
+function init(data) {
     //Agregar Eventos a los botones
     botonesFiltros.forEach((boton) => {
         boton.addEventListener("click", () => {
             boton.classList.toggle("activo");
+            console.log(data);
         });
     });
-    //Traer los datos del .json
-    fetch("../data.json")
-        .then((response) => response.json())
-        .then((data) => mostrarDatos(data));
-});
-
-function mostrarDatos(data) {
     data.forEach((extension) => {
         const card = document.createElement("div");
         card.classList.add("card");
@@ -36,18 +37,24 @@ function mostrarDatos(data) {
         remove.textContent = "Remove";
         const toggleActive = document.createElement("label");
         toggleActive.innerHTML = `
-            <input type="checkbox">
-            <div class="toggle-switch-background">
+            <input id=${extension.name} type="checkbox" ${extension.isActive && "checked"}>
+            <div class="toggle-switch-background" >
                 <div class="toggle-switch-handle"></div>
             </div>
             `;
         toggleActive.classList.add("toggle-switch");
+        toggleActive.addEventListener("change", (e) => {
+            data.filter((extension) => {
+                if (extension.name == e.target.id) {
+                    extension.isActive = !extension.isActive;
+                }
+            });
+        });
         const botones = document.createElement("div");
         botones.appendChild(remove);
         botones.appendChild(toggleActive);
         botones.classList.add("botones");
         card.appendChild(botones);
         container.appendChild(card);
-        console.log(extension);
     });
 }
